f506d847c931ff7b9a71b45a0d9b1d5b5096ba6d
lb <bozhko.olena1123@vu.cdu.edu.ua>
2024-12-25 00:15:04 +0200

    Done the Task1, write the files into report_files folder

--- oop_lab3_exceptions/Program.cs
+++ oop_lab3_exceptions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using static System.Console;
 
 namespace oop_lab3_exceptions
 {
@@ -6,18 +7,21 @@ namespace oop_lab3_exceptions
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             int choice;
 
             do
             {
-                Console.WriteLine("1. Filereader");
+                Console.WriteLine("1. Files Reader");
                 Console.WriteLine("2. Image Reflector");
                 choice = Convert.ToInt32(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("block1");
+                        Console.WriteLine("Running Block1:");
+                        DoTask1();
+                        Console.ReadLine();
                         break;
                     case 2:
                         break;
@@ -27,6 +31,62 @@ namespace oop_lab3_exceptions
 
                 }
             } while (choice != 0);
+        }
+
+        static void DoTask1()
+        {
+            string inputFiles = "../../../../input_files";
+            List<string> noFile = new();
+            List<string> badData = new();
+            List<string> overflow = new();
+
+            List<int> products = new List<int>();
+
+            for (int i = 10; i <= 29; i++)
+            {
+                string file = Path.Combine(inputFiles, $"{i}.txt");
+
+                try
+                {
+                    StreamReader sr = new StreamReader(file);
+                    int first = int.Parse(sr.ReadLine());
+                    int second = int.Parse(sr.ReadLine());
+                    int product = checked(first * second);
+                    products.Add(product);
+                }
+                catch (FileNotFoundException)
+                {
+                    noFile.Add($"{i}.txt");
+                }
+                catch (FormatException)
+                {
+                    badData.Add($"{i}.txt");
+                }
+                catch (OverflowException)
+                {
+                    overflow.Add($"{i}.txt");
+                }
+            }
+            int result = products.Sum() / products.Count;
+            Console.WriteLine($"Середнє значення: {result}");
+
+            WriteListToFIle(noFile, "../../../../report_files/no_file.txt");
+            WriteListToFIle(badData, "../../../../report_files/bad_data.txt");
+            WriteListToFIle(overflow, "../../../../report_files/overflow.txt");
+
+        }
+
+        static void WriteListToFIle(List<string> list, string filePath)
+        {
+            try
+            {
+                File.WriteAllLines(filePath, list);
+                Console.WriteLine($"Файл успішно записано за шляхом: {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Сталася помилка при записі у файл: {ex.Message}");
+            }
         }
     }
 }
