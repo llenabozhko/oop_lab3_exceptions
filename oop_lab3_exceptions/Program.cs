ed5ad97de4491bc19ab68311872342e535cc6652
lb <bozhko.olena1123@vu.cdu.edu.ua>
2024-12-24 23:54:28 +0200

    Initial commit

---
+++ oop_lab3_exceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lab3_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellomworld");
+        }
+    }
+}
