62f5241c9e545f0dc81261e13b9f818368d1656d
lb <bozhko.olena1123@vu.cdu.edu.ua>
2024-12-25 00:03:10 +0200

    Switch-case to choose which task to run

--- oop_lab3_exceptions/Program.cs
+++ oop_lab3_exceptions/Program.cs
@@ -6,7 +6,27 @@ namespace oop_lab3_exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellomworld");
+            int choice;
+
+            do
+            {
+                Console.WriteLine("1. Filereader");
+                Console.WriteLine("2. Image Reflector");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("block1");
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        Console.WriteLine("Exit");
+                        break;
+
+                }
+            } while (choice != 0);
         }
     }
 }
